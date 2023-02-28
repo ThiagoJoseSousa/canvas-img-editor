32be8d6b987530e9cbd80980ed83b9be841aaf7d
ThiagoJoseSousa <imigrationstudy@gmail.com>
2023-02-28 20:39:53 -0300

    resizing fixed

--- canvas.js
+++ canvas.js
@@ -2,16 +2,18 @@ const canvas= document.querySelector('.view__canvas');
 const canvasContainer= document.querySelector('.view__container')
 const canvasRes=document.querySelector('.canvas__size')
 const ctx= canvas.getContext('2d');
+const canvasSaving= document.createElement('canvas');
+const savingCtx = canvasSaving.getContext('2d')   
 
 const toolConfig= document.querySelector('.editor__tools--config')
 
 //make canvas a responsive sive for the section editor__config-tools
-const aspectToggle= document.querySelector('.resizer__aspect')
-const heightInput =document.querySelector('.resizer__input--height')
+const aspectToggle= document.getElementById('resizer__aspect')
+const heightInput =document.getElementById('resizer__input--height')
 const fileInput= document.querySelector('.resizer__file');
-const widthInput =document.querySelector('.resizer__input--width')
-let activeImage, originalWidthToHeightRatio, isDrawing;
-
+const widthInput =document.getElementById('resizer__input--width')
+let activeImage, isDrawing;
+let originalWidthToHeightRatio='1x1';
 const availableRes=document.getElementsByName('ratio')
 availableRes.forEach((res)=>{
     res.addEventListener('click',(e)=>{
@@ -24,6 +26,7 @@ availableRes.forEach((res)=>{
 keepRatioAndResize()
 
 function changeCanvasByCheck(res){
+    originalWidthToHeightRatio=res;
     const index=res.indexOf('x')
     let width = parseInt(res.slice(0, index),10)
     let height = parseInt(res.slice(index+1,),10)
@@ -31,14 +34,15 @@ function changeCanvasByCheck(res){
 }
 
 window.addEventListener('resize', () => {
-    keepRatioAndResize();
-    if(widthInput.value!=='0'){ // img doesnt load after resize 
-        ctx.drawImage(activeImage,0,0, widthInput.value+1, heightInput.value+1)
-    }
+    keepRatioAndResize(canvas.width,canvas.height)
+    // if(widthInput.value!=='0'){ // img doesnt load after resize 
+    //     console.log(widthInput.value)
+    //     ctx.drawImage(activeImage,0,0, widthInput.value+1, heightInput.value+1)
+    // }
 })
 
 // resizeCanvasToAnotherRatio()
-fileInput.addEventListener("change", e=>{
+fileInput.addEventListener("change", (e)=>{
     const reader= new FileReader(); // Web API, not 'fs'
     
     reader.addEventListener("load", ()=>{
@@ -48,18 +52,30 @@ fileInput.addEventListener("change", e=>{
     reader.readAsDataURL(e.target.files[0]);
 })
 
+function divideRatio(){
+    const i=parseInt(originalWidthToHeightRatio.indexOf('x'));
+    const width=parseInt(originalWidthToHeightRatio.slice(0,i))
+    const height=parseInt(originalWidthToHeightRatio.slice(i+1,))
+    return width/height
+}
+
+function storeOldCanvas(){
+    canvasSaving.width=canvas.width;
+    canvasSaving.height=canvas.height
+    savingCtx.drawImage(canvas,0,0)
+}
+
 widthInput.addEventListener("change", ()=>{
-    if (!activeImage)return;
+    // if (!activeImage)return;
 
-    const heightValue= aspectToggle.checked ? widthInput.value / originalWidthToHeightRatio : heightInput.value;
-
+    console.log(heightInput.value, originalWidthToHeightRatio)
+    const heightValue= aspectToggle.checked ? widthInput.value / divideRatio() : heightInput.value;
     resize({width:widthInput.value, height:heightValue})
 })
-
 heightInput.addEventListener("change", ()=>{
-    if (!activeImage)return;
-    
-    const widthValue= aspectToggle.checked ? heightInput.value / originalWidthToHeightRatio : heightInput.value;
+    //if (!activeImage)return;
+    console.log(heightInput.value)
+    const widthValue= aspectToggle.checked ? heightInput.value * divideRatio() : widthInput.value;
     
     
     resize({width:widthValue, height:heightInput.value})
@@ -70,30 +86,36 @@ function openImage(imageSrc){
     activeImage= new Image();
     activeImage.src=imageSrc;
     activeImage.addEventListener("load",()=>{
-        originalWidthToHeightRatio = activeImage.width/activeImage.height;
-
+        originalWidthToHeightRatio = `${activeImage.width}x${activeImage.height}`;
+        console.log(activeImage.width,activeImage.height)
         resize({width:activeImage.width, height:activeImage.height})
     })
 }
+imagem bugando
 
 function resize(dimensions){
 const {width,height}= dimensions
-// canvas.width=width;
-// canvas.height=height;
+storeOldCanvas()
+ canvas.width=width;
+ canvas.height=height;
 widthInput.value=width;
 heightInput.value=height;
+console.log (width,height)
+ctx.drawImage(canvasSaving,0,0)
 
-ctx.drawImage(activeImage,0,0, width, height)
+// ctx.drawImage(activeImage,0,0, width, height)
 }
-
-
+ 
 function keepRatioAndResize(cWidth,cHeight){
-    let width= cWidth? cWidth:1920;
-    let height = cHeight? cHeight:1080;
-let maxWidth, maxHeight, ratio; //different ratio
-getLimitSize()
-getNonStretchRatio()
-resizeCanvas()
+    let width= cWidth? cWidth:1000;
+    let height = cHeight? cHeight:1000;
+    let maxWidth, maxHeight, ratio; //different ratio
+    storeOldCanvas()
+    getLimitSize()
+    getNonStretchRatio()
+    resizeCanvas()
+    ctx.drawImage(canvasSaving,0,0)
+
 function getLimitSize(){
     maxWidth= canvasContainer.clientWidth;
     maxHeight= canvasContainer.clientHeight;
@@ -115,7 +137,7 @@ function resizeCanvas(){
 let brushWidth=document.querySelector('#line-width');
 
 const startDraw= ()=>{
-    if (activeTool==="Drawing"){
+    if (activeTool==="drawing"){
        ctx.beginPath()
         isDrawing=true;
         ctx.lineWidth= brushWidth.value;
